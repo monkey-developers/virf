bb8675b42c7483d1abeb42980b78e2526a1b8d33
Abe Hidek <hidek.abe@outlook.com>
2022-06-09 23:56:09 -0300

    Add flash_scores function

--- src/jokenpo/jokenpogame.py
+++ src/jokenpo/jokenpogame.py
@@ -1,7 +1,8 @@
 from os import path
 from random import randint
+from time import sleep
 from PyQt5 import uic, QtWidgets, QtGui
-
+import threading
 
 
 def play():
@@ -13,22 +14,36 @@ def play():
 
     values = ["rock", "paper", "scissor"]
 
+    def flash_scores(draw, humanWin):
+        if draw: 
+            ui.ScoreHuman.setStyleSheet("color: blue;")
+            ui.ScoreRobot.setStyleSheet("color: blue;")
+        else: 
+            if humanWin: ui.ScoreHuman.setStyleSheet("color: green;")
+            else: ui.ScoreRobot.setStyleSheet("color: red;")
+        sleep(0.250)
+        ui.ScoreHuman.setStyleSheet("color: black;")
+        ui.ScoreRobot.setStyleSheet("color: black;")
 
     def win(isHuman, humanScore, robotScore):
         
         if isHuman: ui.ScoreResult.setText("you win"); humanScore +=1; ui.ScoreHuman.setText(str(humanScore))
         else: ui.ScoreResult.setText("you lose"); robotScore +=1; ui.ScoreRobot.setText(str(robotScore))
 
+        if isHuman: threading.Thread(target=lambda:flash_scores(False, True)).start()
+        else: threading.Thread(target=lambda:flash_scores(False, False)).start()
+
         ui.ScoreHuman.setText(str(humanScore))
         ui.ScoreRobot.setText(str(robotScore))
 
     def turn(humanSelected):
         robotSelected = values[randint(0,2)]
-
+        
         humanScore, robotScore = int(ui.ScoreHuman.text()), int(ui.ScoreRobot.text())
 
         if (humanSelected == robotSelected):
             ui.ScoreResult.setText("draw")
+            threading.Thread(target=lambda:flash_scores(True, False)).start()
             return
         if (humanSelected == "rock"):
             if robotSelected == "scissor": win(isHuman=True, humanScore=humanScore, robotScore=robotScore)
